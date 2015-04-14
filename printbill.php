3c2a09be44759468fb555e7ac84d48dbece72ff6
alaaelgndy <alaaelgndy21@yahoo.com>
2015-04-14 17:28:25 +0200

    h2oom akol w agy

--- printbill.php
+++ printbill.php
@@ -4,7 +4,7 @@
 		<div class="col-xs-12">
 			<div class="box">
 				<div class="box-header">
-					<h3 class="box-title">فواتير العملاء</h3>                                    
+					<h3 class="text-center box-title" >فواتير العملاء</h3>                                    
 				</div><!-- /.box-header -->
 				
 				<div class="box-body table-responsive">
@@ -15,15 +15,14 @@
 							<div class="col-xs-6">
 							</div>
 						</div>
-						<table id="example1" class="table table-bordered table-striped dataTable" aria-describedby="example1_info">
+						<table id="example1" class="table table-bordered table-striped dataTable" dir="rtl" aria-describedby="example1_info">
 							<thead>
 								<tr role="row">
-									<th>المسلسل</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 277px;">المنتج</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 248px;">الكمية</th>
+									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 40px;">المسلسل</th>
+									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 170px;">المنتج</th>
+									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 170px;">الكمية</th>
 									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 159px;">السعر</th>
 									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 115px;">الاجمالى</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 159px;">التاريخ</th>
 								</tr>
 							</thead>
 							<tbody role="alert" aria-live="polite" aria-relevant="all"><tr class="odd">
@@ -49,7 +48,7 @@
 									<td class=''>$quantity</td>
 									<td class=''>$price</td>
 									<td class=''>$total</td>
-									<td class=''>$date</td>
+						
 									</tr>";
 									$i++;
 								}
