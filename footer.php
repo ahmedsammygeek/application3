cad59addc22e1f81639a614fdcc2cc5df12615f4
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-04-14 23:15:56 +0200

    =add search table to show suppliers page

--- footer.php
+++ footer.php
@@ -8,17 +8,12 @@
 <script src="js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
 <!-- Bootstrap -->
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
-<!-- Morris.js charts -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<!-- Sparkline -->
-<script src="js/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
-<!-- jvectormap -->
-<script src="js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
-<script src="js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
-<!-- jQuery Knob Chart -->
-<script src="js/plugins/jqueryKnob/jquery.knob.js" type="text/javascript"></script>
+
+
 <!-- daterangepicker -->
 <script src="js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<script src="js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+<script src="js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
 <script src="js/AdminLTE/app.js" type="text/javascript"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="js/select.js"></script>
@@ -80,14 +75,7 @@ $.ajax({
 </script>
 <script type="text/javascript">
 $(function() {
-  // $('#example1').dataTable({
-  //   "bPaginate": true,
-  //   "bLengthChange": false,
-  //   "bFilter": false,
-  //   "bSort": true,
-  //   "bInfo": true,
-  //   "bAutoWidth": false
-  // });
+  $('#example1').dataTable();
 });
 
 
