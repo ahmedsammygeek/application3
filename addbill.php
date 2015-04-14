e42fd7db8b210f5922ff93e3e975d53eaeec411a
alaaelgndy <alaaelgndy21@yahoo.com>
2015-04-14 14:53:25 +0200

    delete comments in addbill.php

--- addbill.php
+++ addbill.php
@@ -7,6 +7,7 @@ if(!isset($_SESSION['logged']) || $_SESSION['logged'] != 'true') {
 if (isset($_POST['submit'])) {
 	$client=$_POST['client'];
 	$date=date("Y-m-d");
+	$bill_num=rand(2,100000);
 	$total_of_bill=array();
 	for ($i=0; $i < count($_POST['quantity']); $i++) { 
 		$quantity=htmlspecialchars($_POST['quantity'][$i]);
@@ -22,7 +23,6 @@ if (isset($_POST['submit'])) {
 		$conn->beginTransaction();
 		try {
 	//bill number
-			$bill_num=rand(2,100);
 
 //check if this product's price add before or no .
 // 			$query=$conn->query("SELECT price FROM bills WHERE product_id=$product && client_id=$client");
@@ -74,74 +74,6 @@ array_push($total_of_bill, $total);
 	var_dump($total_of_bill); die();
 	header("location: bill.php?msg=data_inserted");die();
 } // end of if isset
-
-// //check price2 exist or no
-// 	$query2=$conn->query("SELECT price from bills WHERE product_id=$product2 && client_id=$client");
-// if ($query2->rowCount() != 0) {
-// 	$result2=$query2->fetch(PDO::FETCH_ASSOC);
-// 	extract($result2);
-// 	$price2=$price;
-// }
-// //total = price * quantity
-// $total2=bcmul($quantity2, $price2);
-// if (empty($quantity2) || empty($price2)) {
-// 	header("location: bill.php?msg=empty_data");die();
-// }
-// //insert data about second product
-// $query3=$conn->prepare("INSERT INTO bills VALUES ('',?,?,?,?,?,?,?) ");
-// $query3->bindValue(1,$client,PDO::PARAM_INT);
-// $query3->bindValue(2,$product2,PDO::PARAM_INT);
-// $query3->bindValue(3,$quantity2,PDO::PARAM_INT);
-// $query3->bindValue(4,$date,PDO::PARAM_INT);
-// $query3->bindValue(5,$price2,PDO::PARAM_INT);
-// $query3->bindValue(6,$bill_num,PDO::PARAM_INT);
-// $query3->bindValue(7,$total2,PDO::PARAM_INT);
-// if ($query3->execute()) {
-// 	//update the realty quantity in products table
-// 	$update1=$conn->prepare("UPDATE products SET quantity='quantity-$quantity2' WHERE id='$product2' ");
-// 	$update1->execute();
-// }
-// //check price of product3 exist before or no ..
-// 	$query4=$conn->query("SELECT price from bills WHERE product_id=$product3 && client_id=$client");
-// if ($query4->rowCount() != 0) {
-// 	$result4=$query4->fetch(PDO::FETCH_ASSOC);
-// 	extract($result4);
-// 	$price3=$price;
-// }
-// //total=price * quantity
-// $total3=bcmul($quantity3, $price3);
-// if (empty($quantity3) || empty($price3)) {
-// 	header("location: bill.php?msg=empty_data");die();
-// }
-// //insert data about product3
-// $query5=$conn->prepare("INSERT INTO bills VALUES ('',?,?,?,?,?,?,?) ");
-// $query5->bindValue(1,$client,PDO::PARAM_INT);
-// $query5->bindValue(2,$product3,PDO::PARAM_INT);
-// $query5->bindValue(3,$quantity3,PDO::PARAM_INT);
-// $query5->bindValue(4,$date,PDO::PARAM_INT);
-// $query5->bindValue(5,$price3,PDO::PARAM_INT);
-// $query5->bindValue(6,$bill_num,PDO::PARAM_INT);
-// $query5->bindValue(7,$total3,PDO::PARAM_INT);
-// if ($query5->execute()) {
-// 	//update the realty quantity for product3 in products table
-// 	$update2=$conn->prepare("UPDATE products SET quantity='quantity-$quantity3' WHERE id='$product3' ");
-// 	if ($update2->execute()) {
-// 		header("location: showbill.php?msg=data_inserted");die();
-// 	}
-
-// }
-// header("location: bill.php?msg=error_data");die();
-
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
