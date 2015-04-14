d6e49b9a57e573291b59496654ef4dffb94b2ae0
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-04-15 00:17:56 +0200

    =add search table to show suppliers bills page

--- showsupplierbill.php
+++ showsupplierbill.php
@@ -46,37 +46,22 @@
 				<div class="box-body table-responsive">
 					<div id="example1_wrapper" class="dataTables_wrapper form-inline" role="grid">
 						<div class="row">
-							<div class="col-xs-6">
-								</div>
-					<div class="col-xs-6">
-				</div>
-						</div>
-						<table id="example1" class="table table-bordered table-striped dataTable" aria-describedby="example1_info">
-							<thead>
-								<tr role="row">
-									<th>المسلسل</th>
-									<th class="sorting_asc" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 189px;">اسم المورد</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 277px;">المنتج المشترى</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 248px;">الكمية</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 159px;">السعر</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 159px;">التاريخ</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 115px;">خيارات</th>
-								</tr>
-							</thead>
+							<div class="col-xs-12">
 
-							<tfoot>
-								<tr role="row">
-									<th>المسلسل</th>
-									<th class="sorting_asc" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 189px;">اسم المورد</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 277px;">المنتج المشترى</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 248px;">الكمية</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 159px;">السعر</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 159px;">التاريخ</th>
-									<th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 115px;">خيارات</th>
-								</tr>
-							</tfoot>
-							<tbody role="alert" aria-live="polite" aria-relevant="all"><tr class="odd">
-								<?php 
+  <table id="example1" class="table table-bordered ">
+                                        <thead>
+                                            <tr>
+                                                <th>المسلسل</th>
+                                                <th>اسم المورد</th>
+                                                <th>المنتج المشترى</th>
+                                                <th>الكمية</th>
+                                                <th>السعر</th>
+                                                <th>التاريخ</th>
+                                                <th>خيارات</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                       <?php 
 								require 'connection.php'; 
 								$sql="SELECT * FROM bill_products";
 								$query=$conn->query($sql);
@@ -103,10 +88,15 @@
 									$i++;
 								}
 								?>
-							</tbody>
-						</table>
+                                        </tbody>
+                                        
+                                    </table>
 
 
+
+								
+							</div>
+						</div>
 					</div><!-- /.box-body -->
 				</div>
 			</div>
